cea6eae2c3803eec0dad96df6a520d3641bfc1be
rosa161 <rosarosa161@gmail.com>
2025-09-02 09:14:06 +0800

    0811_1

---
+++ js0902/App0902a.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='wrap'>
+        排序：
+        <input type="checkbox" />
+        <br />
+        搜尋：
+        <input type="text" />
+
+        {/* 表格 */}
+        <table>
+            
+        </table>
+    </div>
+  )
+}
+
+export default App
